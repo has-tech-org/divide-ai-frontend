80deb31f4f5e5e4507632ed98f1e3fc07248dea8
Yale Araujo <yale850@gmail.com>
2025-07-11 14:46:46 -0300

    Initialize project with Vite, React, and Tailwind CSS setup. Added essential configuration files including .gitignore, ESLint, TypeScript, and Vite config. Created initial application structure with components, layouts, and utility functions. Included SVG assets and basic styling with Tailwind CSS.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <span className='text-red-500'>teste 123</span>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
