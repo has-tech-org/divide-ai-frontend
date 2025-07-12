0c86ecfad87c9fef74786e2e51f37575f011d37a
Yale Araujo <yale850@gmail.com>
2025-07-12 14:33:20 -0300

    Add React Query and Axios integration to the application
    - Introduced @tanstack/react-query for data fetching and state management.
    - Created a new api.ts file to configure Axios with a base URL for API requests.
    - Updated App component to wrap routes with QueryClientProvider for React Query context.
    - Added a react-query.ts file to initialize the QueryClient instance for managing query states.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 import "./App.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export const App = () => {
 	return (
-		<BrowserRouter>
-			<AppRoutes />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 };
