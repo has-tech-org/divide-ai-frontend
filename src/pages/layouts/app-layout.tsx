153b84aa3ce51b4922158729fa954ca27c4be296
Yale Araujo <yale850@gmail.com>
2025-07-19 21:54:17 -0300

    Add response interceptor in AppLayout for unauthorized access handling
    - Implemented an Axios response interceptor in AppLayout to navigate to the home page on 401 errors.
    - Utilized useEffect to manage the interceptor lifecycle, ensuring proper cleanup on component unmount.

--- src/pages/layouts/app-layout.tsx
+++ src/pages/layouts/app-layout.tsx
@@ -1,8 +1,30 @@
-import { Outlet } from "react-router";
+import { isAxiosError } from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { api } from "@/lib/api";
 
 export const AppLayout = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const interceptorId = api.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				if (isAxiosError(error)) {
+					if (error.status === 401) {
+						navigate("/");
+					}
+				}
+			},
+		);
+
+		return () => {
+			api.interceptors.response.eject(interceptorId);
+		};
+	}, [navigate]);
+
 	return (
 		<div className="flex h-full bg-zinc-900">
 			<SidebarProvider>
