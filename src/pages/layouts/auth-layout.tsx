08ebb911c5a765427ff807b3a3495b1ff9234a34
Yale Araujo <yale850@gmail.com>
2025-07-19 21:36:35 -0300

    Update AuthLayout component for improved layout and styling
    - Adjusted the AuthLayout component to center content and enhance responsiveness.
    - Modified the layout structure to ensure better alignment and visual appeal on larger screens.

--- src/pages/layouts/auth-layout.tsx
+++ src/pages/layouts/auth-layout.tsx
@@ -4,15 +4,15 @@ import { Logo } from "@/components/ui/logo";
 
 export const AuthLayout = () => {
 	return (
-		<div className="flex h-screen bg-zinc-900">
-			<div className="max-w-3xl 2xl:max-w-6xl w-full h-full">
+		<div className="flex justify-center h-screen bg-zinc-900">
+			<div className="flex-1 hidden xl:block">
 				<img
 					src={imageBanner}
 					alt="logo"
 					className="w-full h-full object-cover"
 				/>
 			</div>
-			<div className="flex-1">
+			<div className="max-w-2xl w-full">
 				<div className="max-w-lg px-4 w-full mx-auto flex flex-col justify-center items-center h-full gap-10">
 					<Outlet />
 					<footer className="flex flex-col items-center">
